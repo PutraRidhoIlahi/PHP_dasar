5b2d478cddd97b6a19814b06de6416a03bdfe051
NeoPhyte <aldijinkpro27@gmail.com>
2023-11-16 18:46:22 +0700

    membuat looping pertama

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>loping</title>
+</head>
+<body>
+  <h1>Berlatih Looping</h1>
+
+<?php
+for ($i = 1; $i <= 20; $i++)
+{
+  echo "I Love PHP $i \n";
+}
+?>
+
+</body>
+</html>
+
