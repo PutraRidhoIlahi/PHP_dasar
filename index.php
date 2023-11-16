91ba4892b7ff482e3767d604ad708efbcf345345
NeoPhyte <aldijinkpro27@gmail.com>
2023-11-16 21:53:15 +0700

    membuat looping array module

--- index.php
+++ index.php
@@ -19,7 +19,21 @@ for ($i = 20; $i >= 2; $i-=2)
 {
     echo "$i I LOVE PHP \n";
 }
+?>
 
+<!-- looping array module -->
+<h3> soal nomor </h3>
+<?php
+// Array angka
+$numbers = array(18, 45, 29, 61, 47, 34);
+$rest = array();
+foreach ($numbers as $nilai) {
+    $rest[] = $nilai % 5;
+}
+echo "Array Rest: ";
+foreach ($rest as $hasil) {
+    echo $hasil . " ";
+}
 ?>
 
 </body>
